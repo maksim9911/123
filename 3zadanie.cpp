32957d6e1a426efc320bc41bc2262ea78bf6492d
maksim9911 <flaix888@gmail.com>
2026-04-07 23:23:06 +0300

    Add files via upload

---
+++ 3zadanie.cpp
@@ -0,0 +1,38 @@
+#include <iostream> // через цикл for
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 21; i += 2) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
+
+
+#include <iostream> // через цикл while
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 21) {
+        cout << i << " ";
+        i += 2;
+    }
+    return 0;
+}
+
+
+
+
+#include <iostream> // через цикл do while 
+using namespace std;
+
+int main() {
+    int i = 1;
+    do {
+        cout << i << " ";
+        i += 2;
+    } while (i <= 21);
+    return 0;
+}
