52bd6bd79fab5bd78588a54d3f5929f92547dbea
maksim9911 <flaix888@gmail.com>
2026-04-08 10:40:43 +0300

    Update 1zadanie.cpp

--- 1zadanie.cpp
+++ 1zadanie.cpp
@@ -1,16 +1,16 @@
 #include <iostream> // программа определяет, где находится точка (х,у) относительно заштрихованной области, которая на рисунке.
 using namespace std;
-
+ 
 int main() {
     double x, y;
     cin >> x >> y;
-    
-    if (x == -2 || x == 3 || y == x)
+ 
+    if (x>=-23 && x<=0 && y>=x && y<=0)
         cout << "На границе";
-    else if (x > -2 && x < 3 && y > x)
+    else if (x > -23 || y == 0 || y == x)
         cout << "Да";
     else
         cout << "Нет";
-    
+ 
     return 0;
 }
